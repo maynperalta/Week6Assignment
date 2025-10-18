b05f5aa3bab7b15d804b851a5cea8ba733c57118
Maynard Peralta <maynperalta@gmail.com>
2025-10-17 20:56:27 -0600

    Added class and constructor

---
+++ Week6Assignment/src/week6Assingment/Main.java
@@ -0,0 +1,39 @@
+package week6Assingment;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+
+public class Vehicle {
+	private int vehicleYear;
+	private double mileage;
+	private String vehicleId;
+	private String vehicleColor;
+	private String vehicleMake;
+	private String vehicleModel;
+	private String drivetrain;
+	private String vehicleCategory;
+	
+	public Vehicle(int year, double miles, String id, String color, String make, String model, String drive, String category) {
+		vehicleYear = year;
+		mileage = miles;
+		vehicleId = id;
+		vehicleColor = color;
+		vehicleMake = make;
+		vehicleModel = model;
+		drivetrain = drive;
+		vehicleCategory = category;
+	}
+}
+
+
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
